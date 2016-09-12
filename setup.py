6c8c90b39a37495836fdb406580db67763ef8226
rachmadaniHaryono <foreturiga@gmail.com>
2016-09-12 16:36:46 +0800

    add tox for tox test.

--- setup.py
+++ setup.py
@@ -3,17 +3,60 @@
 
 import os
 from setuptools import setup
+from setuptools.command.test import test
 
 
-here = os.path.dirname(__file__)
-with open(os.path.join(here, 'readme.md')) as _fo:
-    LONG_DESCRIPTION = _fo.read()
+# taken example from
+# https://coderwall.com/p/qawuyq/use-markdown-readme-s-in-python-modules
+SHORT_DESCRIPTION = 'Downloads images from sub-reddits of reddit.com.'
+# fall back when long description is not found.
+LONG_DESCRIPTION = SHORT_DESCRIPTION
+if os.path.exists('README.txt'):
+    LONG_DESCRIPTION = open('README.txt').read()
 
+TESTS_REQUIRE = [
+    'mock',
+    'pytest',
+    'tox'
+]
+
+
+class Tox(test):
+    """Extend setuptools/distribute test command.
+
+    It is used to trigger a test run when python setup.py test is issued.
+
+    Taken from:
+    https://tox.readthedocs.io/en/latest/example/basic.html
+    """
+
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        """initialize_options."""
+        test.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        """finalize options."""
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """run tests."""
+        # import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        tox.cmdline(args=args)
 
 setup_kwargs = dict(
     name='redditdownload',
     version='1.5',
-    description='',  ## XX
+    description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     # classifiers=[],
     # keywords='...,...',
@@ -29,6 +72,8 @@ setup_kwargs = dict(
     install_requires=[
         # Actually, most of the dependencies are optional.
     ],
+    tests_require=TESTS_REQUIRE,
+    cmdclass={'test': Tox},
     extras_require={
         'recommended': [
             'beautifulsoup4', 'lxml', 'html5lib',
