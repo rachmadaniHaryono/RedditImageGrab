b4063802e44236aad228fc9e2bd389bc3a072ce1
rachmadaniHaryono <foreturiga@gmail.com>
2018-01-19 06:42:21 +0800

    chg: test: [test_reddit] update module
    - upgrade to python3 syntax and library
    - use vcrpy to cache requests
    - use pytet parametrize

--- tests/test_reddit.py
+++ tests/test_reddit.py
@@ -1,26 +1,22 @@
 """test reddit module."""
-import json
-try:  # py3
-    from unittest import mock
-except ImportError:  # py2
-    import mock
-    from urllib2 import HTTPError
+from unittest import mock
+from urllib.error import HTTPError
+import itertools
+import logging
 
 import pytest
+import vcr
 
 from redditdownload.reddit import getitems
 
 
-def test_empty_string():
-    """test with empty string input."""
-    res = getitems('')
-    assert isinstance(res, list)
-    assert len(res) > 0
+logging.basicConfig()
+vcr_log = logging.getLogger("vcr")
+vcr_log.setLevel(logging.INFO)
 
 
-@mock.patch('redditdownload.reddit.urlopen')
-@mock.patch('redditdownload.reddit.Request')
-def test_empty_string_mock(mock_requests, mock_urlopen):
+@vcr.use_cassette()
+def test_empty_string(caplog):
     """test empty string but with mocking external dependencies.
 
     it will result the following url which
@@ -28,121 +24,118 @@ def test_empty_string_mock(mock_requests, mock_urlopen):
     which will redirect to json version of this url
     https://www.reddit.com/subreddits
     """
-    expected_url = 'http://www.reddit.com/r/.json'
-    expected_headers = {'User-Agent': 'RedditImageGrab script.'}
-    mock_resp = mock.Mock()
-    mock_url_dict = {'url': "http://example.com/a.jpg"}
-    mock_data = [{'data': mock_url_dict}]
-    mock_resp.read.return_value = json.dumps({'data': {'children': mock_data}})
-    mock_urlopen.return_value = mock_resp
-
     res = getitems("")
-
-    assert len(res) == 1
-    assert res[0] == mock_url_dict
-    mock_requests.assert_called_once_with(expected_url, headers=expected_headers)
-    assert len(mock_urlopen.mock_calls) == 2
-    mock_urlopen.assert_has_calls([
-        mock.call(mock_requests.return_value),
-        mock.call().read(),
-    ])
+    assert caplog.records[0].name == 'redditdownload.reddit'
+    assert caplog.records[0].msg == 'url:http://www.reddit.com/r/.json'
+    assert len(res) == 25
+    assert res[0]['url'] == '/r/AskReddit/'
 
 
-@mock.patch('redditdownload.reddit.urlopen')
-@mock.patch('redditdownload.reddit.Request')
-def test_sort_type(mock_requests, mock_urlopen):
-    """test sort_type."""
-    mock_resp = mock.Mock()
-    mock_url_dict = {'url': "http://example.com/a.jpg"}
-    mock_data = [{'data': mock_url_dict}]
-    mock_resp.read.return_value = json.dumps({'data': {'children': mock_data}})
-    mock_urlopen.return_value = mock_resp
-
+def get_parameter_for_test_sort_type():
     # sort_type none, input is multireddit
-    sort_type = None
-    reddit_input = 'some_user/m/some_multireddit'
-    expected_url = 'http://www.reddit.com/user/some_user/m/some_multireddit.json'
-    res = getitems(reddit_input, reddit_sort=sort_type, multireddit=True)
-    # test
-    mock_requests.assert_called_once_with(expected_url, headers=mock.ANY)
-
-    # starting with none sort_type
-    mock_requests.reset_mock()
-    sort_type = None
-    expected_url = 'http://www.reddit.com/r/cats.json'
-    res = getitems('cats', reddit_sort=sort_type)
-    # test
-    mock_requests.assert_called_once_with(expected_url, headers=mock.ANY)
-
+    res = [
+        # sort_type none, input is multireddit
+        (
+            {'subreddit': 'some_user/m/some_multireddit', 'reddit_sort': None, 'multireddit': True, },
+            {
+                'record_msgs': [
+                    'url:http://www.reddit.com/user/some_user/m/some_multireddit.json',
+                    "data: {'message': 'Not Found', 'error': 404}"
+                ],
+                'error': KeyError,
+            }
+        ),
+        # starting with none sort_type
+        (
+            {'subreddit': 'cats', 'reddit_sort': None, },
+            {'record_msgs': ['url:http://www.reddit.com/r/cats.json'], }
+        ),
+    ]
     # test with sort type
     for sort_type in ['hot', 'new', 'rising', 'controversial', 'top', 'gilded']:
-        mock_requests.reset_mock()
-        res = getitems('cats', reddit_sort=sort_type)
-        expected_url = 'http://www.reddit.com/r/cats/{}.json'.format(sort_type)
-        mock_requests.assert_called_once_with(expected_url, headers=mock.ANY)
+        res.append((
+            {'subreddit': 'cats', 'reddit_sort': sort_type},
+            {'record_msgs': ['url:http://www.reddit.com/r/cats/{}.json'.format(sort_type)]}
+        ))
 
-    # test with advanced_sort
-    for sort_type in ['controversial', 'top']:
-        for time_limit in ['hour', 'day', 'week', 'month', 'year', 'all']:
+    for sort_type in ['controversial']:  # , 'top']:
+        for time_limit in ['hour']:  # , 'day', 'week', 'month', 'year', 'all']:
             reddit_sort = sort_type + time_limit
-            mock_requests.reset_mock()
-            url_format = 'http://www.reddit.com/r/cats/{0}.json?sort={0}&t={1}'
-            expected_url = url_format.format(sort_type, time_limit)
+            res.append((
+                {'subreddit': 'cats', 'reddit_sort': reddit_sort},
+                {'record_msgs': [
+                    'url:http://www.reddit.com/r/cats/{0}.json?sort={0}&t={1}'.format(
+                        sort_type, time_limit
+                    ),
+                ]}
+            ))
+    return res
 
-            res = getitems('cats', reddit_sort=reddit_sort)
-            assert len(res) == 1
-            assert res[0] == mock_url_dict
-            mock_requests.assert_called_once_with(expected_url, headers=mock.ANY)
+
+@pytest.mark.parametrize("test_input, data", get_parameter_for_test_sort_type())
+@vcr.use_cassette(record_mode='new_episodes')
+def test_sort_type(caplog, test_input, data):
+    """test sort_type."""
+    error = data.get('error', None)
+    if error:
+        with pytest.raises(error):
+            getitems(**test_input)
+    else:
+        getitems(**test_input)
+    filtered_records = list(filter(lambda x: x.name == 'redditdownload.reddit', caplog.records))
+    record_msgs = [x.msg for x in filtered_records]
+    assert record_msgs == data['record_msgs']
 
 
-@mock.patch('redditdownload.reddit.urlopen')
-@mock.patch('redditdownload.reddit.Request')
-def test_advanced_sort_and_last_id(mock_requests, mock_urlopen):
-    """test for advanced sort and last id."""
+def get_parameter_for_test_advanced_sort_and_last_id():
     last_id = '44h81z'
-    mock_resp = mock.Mock()
-    mock_url_dict = {'url': "http://example.com/a.jpg"}
-    mock_data = [{'data': mock_url_dict}]
-    mock_resp.read.return_value = json.dumps({'data': {'children': mock_data}})
-    mock_urlopen.return_value = mock_resp
-
-    # test with advanced_sort
-    for sort_type in ['controversial', 'top']:
-        for time_limit in ['hour', 'day', 'week', 'month', 'year', 'all']:
-            mock_requests.reset_mock()
-
-            reddit_sort = sort_type + time_limit
-            url_format = 'http://www.reddit.com/r/cats/{0}.json?after=t3_{2}&sort={0}&t={1}'
-            expected_url = url_format.format(sort_type, time_limit, last_id)
-
-            res = getitems('cats', reddit_sort=reddit_sort, previd=last_id)
-            assert len(res) == 1
-            assert res[0] == mock_url_dict
-            mock_requests.assert_called_once_with(expected_url, headers=mock.ANY)
+    time_limits = ['hour', 'day', 'week', 'month', 'year', 'all']
+    test_input = itertools.product(['controversial', 'top'], time_limits)
+    templ = 'http://www.reddit.com/r/cats/{0}.json?after=t3_{2}&sort={0}&t={1}'
+    res = [
+        [
+            (x[0], x[1], last_id),
+            templ.format(x[0], x[1], last_id)
+        ]
+        for x in test_input
+    ]
+    return res
 
 
-@mock.patch('redditdownload.reddit.Request')
-def test_raise_error_on_request(mock_requests):
+@pytest.mark.parametrize("test_input, data", get_parameter_for_test_advanced_sort_and_last_id())
+@vcr.use_cassette(record_mode='new_episodes')
+def test_advanced_sort_and_last_id(caplog, test_input, data):
+    """test for advanced sort and last id."""
+    reddit_sort = test_input[0] + test_input[1]
+    res = getitems('cats', reddit_sort=reddit_sort, previd=test_input[2])
+    filtered_records = \
+        list(filter(lambda x: x.name == 'redditdownload.reddit', caplog.records))
+    record_msgs = [x.msg for x in filtered_records]
+    assert record_msgs == ['url:{}'.format(data)]
+    assert len(res) in [25, 0]
+
+
+@mock.patch('redditdownload.reddit.requests.get')
+def test_raise_error_on_request(m_get):
     """test when error raised on requests."""
     errors = [
         HTTPError(None, 404, 'mock error', None, None), KeyboardInterrupt]
     for error in errors:
-        mock_requests.side_effect = error
-
+        m_get.side_effect = error
         with pytest.raises(SystemExit):
             getitems('cats')
 
 
-@mock.patch('redditdownload.reddit.Request')
-def test_value_error_on_request(mock_requests):
+@mock.patch('redditdownload.reddit.requests.get')
+def test_value_error_on_request(m_get):
     """test value error on requests."""
-    mock_requests.side_effect = ValueError('Mock error')
+    m_get.side_effect = ValueError('Mock error')
     with pytest.raises(ValueError):
         getitems('cats')
 
     # value error contain specific message which raise other error.
     msg = 'No JSON object could be decoded'
-    mock_requests.side_effect = ValueError(msg)
+    m_get.side_effect = ValueError(msg)
     with pytest.raises(SystemExit):
         getitems('cats')
 
