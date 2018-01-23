00a70da05bdd45ccfbece8a890261950d34958cf
rachmadaniHaryono <foreturiga@gmail.com>
2018-01-23 20:11:17 +0800

    chg: dev: [forms] new fields for IndexForm

--- redditdownload/forms.py
+++ redditdownload/forms.py
@@ -1,11 +1,13 @@
 """Forms module."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Optional
 
 
 class IndexForm(FlaskForm):
     """Form for index."""
     subreddit = StringField('subreddit', validators=[DataRequired()])
-    sort_mode = StringField('sort_mode', validators=[Optional])
+    sort_mode = StringField('sort mode', validators=[Optional])
+    time_mode = StringField('time mode', validators=[Optional])
+    per_page = IntegerField('per page', validators=[Optional], default=0)
     disable_cache = BooleanField(validators=[Optional()])
